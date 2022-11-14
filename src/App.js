2a084a2dbbbfb7377386b6b57b9a7beed37e68cd
Jonathan Fenoud-Viard <jonathan.fenoudviard@ecole404.fr>
2022-11-14 13:36:56 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Form from './Form'
 import './App.css';
 
-function App() {
+
+function App(){
+  const [todos, setTodos] = useState([])
+  const toggleComplete = (i) => 
+    setTodos(
+      todos.map(
+        (todo, k) => 
+        k === i 
+        ? {
+          ...todo,
+          complete: !todo.complete
+        }
+      : todo
+      )
+    )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>TODO LIST</h1>
+      <p>Entrer votre tâche et appuyer sur <b>ENTRER</b> pour l'ajouter</p>
+      <p>Pour valider la tâche <b>CLIQUER DESSUS</b> pour la barrer</p>
+      <Form 
+        onSubmit={text => setTodos([{text, complete: false}, ...todos])}
+      />
+      <div class="list">
+        {todos.map(({text, complete}, i) => (
+        <div key={text} onClick={() => toggleComplete(i)}
+        style={{
+          textDecoration: complete ? 'line-through' : ""
+        }}
         >
-          Learn React
-        </a>
-      </header>
+        {text}
+        </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}>Reset</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Form.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";  
+
+const useInputValue = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+
+    return{
+        value,
+        onChange: (e) => setValue(e.target.value),
+        resetValue: () =>setValue(" ")
+    }
+}
+
+function Form({onSubmit}){
+    const {resetValue,...text} = useInputValue("");
+
+    return (
+        <form 
+            onSubmit={e => {
+            e.preventDefault()
+            onSubmit(text.value)
+            resetValue();
+        }}>
+            <input {...text} />
+        </form>
+    )
+}
+
+export default Form;
